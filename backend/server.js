bd916e67cf5c1d4fcffcdb1874d0a490ef5436a3
ivan954 <ivan3.eng@gmail.com>
2022-12-18 16:03:08 +0200

    save

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import userRoutes from './routes/userRoutes.js'
-import { errorHandler, notFound } from './midleware/errorMiddleware.js'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./midleware/errorMiddleware.js";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('API is runing.....')
-})
+app.get("/", (req, res) => {
+  res.send("API is runing.....");
+});
 
-app.use('/api/users', userRoutes)
+app.use("/api/users", userRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
-	PORT,
-	console.log(`server runing in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+  PORT,
+  console.log(`server runing in  ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
